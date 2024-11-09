fdd07805a6ce93cbebc68b9a7f8a573e1dec5d0f
Macapps13 <ben.alvaro@icloud.com>
2024-11-10 08:45:22 +1100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from nba_api.stats.static import players as p 
+from nba_api.stats.endpoints import playercareerstats as pcs
 import random as rand
 #Imports Neccessary Libraries
 
-players = p.get_players()
+players = p.get_active_players()
 
 numPlayers = len(players)
 
 randomPlayer = players[rand.randint(1, numPlayers-1)]
 
+career = pcs.PlayerCareerStats(player_id=randomPlayer['id'])
+
 print(randomPlayer['full_name'])
+playerCareer = career.get_normalized_dict()
+playerCareerPoints = playerCareer.CareerTotalsRegularSeason.get('PTS')
+
+print(playerCareerPoints)
