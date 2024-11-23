a1469bbd98ef0c8b3c1cb14bd323d139bba67341
Macapps13 <ben.alvaro@icloud.com>
2024-11-23 19:57:49 +1100

    modified:   main.js

--- main.js
+++ main.js
@@ -27,3 +27,13 @@ var date = now.getDate();
 
 document.getElementById("today").innerHTML = "Today is " + day + ", " + date + "/" + (month+1) + "/" + year;
  
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataTotals/name/LeBron James", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  console.log(response.text())
